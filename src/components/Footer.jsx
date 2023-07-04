eb26f7056437e6b18e17ebb18b84f5ca67deba45
Bittu <bittukumar1181@gmail.com>
2023-07-04 13:12:37 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Techie Spark</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/bittu-kumar-7600a51b4/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://instagram.com/b2kumar.810" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/B2-Kumar" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
